03cc3bad3787249c05a5d04c9653335fefb88de1
Frozenlily <njj1997@outlook.com>
2019-01-31 13:03:04 +0800

    Update LevelDetailsAlt.cs

--- Project Risen/Assets/_Scripts/Levels/LevelDetailsAlt.cs
+++ Project Risen/Assets/_Scripts/Levels/LevelDetailsAlt.cs
@@ -20,7 +20,7 @@ public class LevelDetails : MonoBehaviour
 		public int num;
 		public Vector3 pos;
 	}
-  public Lane[] lanes;
+	public Lane[] lanes;
   
 	private float laneWidth; //Pixel width of lane.
 	private float heightFactor; //Unit of measurement; 0 - Bottom of Screen; 100 - Top of Screen. (Used for damage calculations)
@@ -36,32 +36,33 @@ public class LevelDetails : MonoBehaviour
 	{
 		laneWidth = Screen.width/laneQty;
 		heightFactor = Screen.height/100;
+		
+		lanes = new Lane[laneQty]
     
-    lanes = new Lane[laneQty]
-    
-    //Updates lane details accordingly.
-    var i= 1;
-    foreach (Lane lane in lanes)
-    {
-      lane.num = i;
-      lane.pos = i*LaneWidth/2;
-      i++
-    }
+		//Updates lane details accordingly.
+		var i= 1;
+		foreach (Lane lane in lanes)
+		{
+			lane.num = i;
+			lane.pos = i*LaneWidth/2;
+			i++
+		}
 	}
+	
 	public Vector3 LaneWidth
 	{
-    get 
-    {
-    	Vector3 a = Camera.main.ScreenToWorldPoint(Vector3.zero);
-		  Vector3 b = Camera.main.ScreenToWorldPoint(new Vector3(laneWidth, 0, 0));
-		  return (a + b);
-    }
+		get 
+		{
+			Vector3 a = Camera.main.ScreenToWorldPoint(Vector3.zero);
+			Vector3 b = Camera.main.ScreenToWorldPoint(new Vector3(laneWidth, 0, 0));
+			return (a + b);
+		}
 	}
 	public Vector3 HeightFactor
 	{
-    get
-    {
-		  return Camera.main.ScreenToWorldPoint(new Vector3(0, heightFactor, 0));
-    }
+		get
+		{
+			return Camera.main.ScreenToWorldPoint(new Vector3(0, heightFactor, 0));
+		}
 	}
 }
