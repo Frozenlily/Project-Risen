5c17463c077bbeed0a2ede3ff965355439c57eaa
Frozenlily <njj1997@outlook.com>
2019-01-31 12:13:40 +0800

    Test script.
    Use in conjunction with test script for player controller.

---
+++ Project Risen/Assets/_Scripts/Levels/LevelDetailsAlt.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelDetails : MonoBehaviour 
+{
+	/*---------------------------------------------------------- Variables ----------------------------------------------------------- */
+	public enum LevelTypes
+	{
+		Normal,
+		Chase,
+		Boss
+	}
+	[SerializeField] private LevelTypes levelType;
+	[SerializeField] private float levelTime;
+	[SerializeField] private float laneQty; //Number of lanes.
+  
+	public struct Lane
+	{
+		public int num;
+		public Vector3 pos;
+	}
+  public Lane[] lanes;
+  
+	private float laneWidth; //Pixel width of lane.
+	private float heightFactor; //Unit of measurement; 0 - Bottom of Screen; 100 - Top of Screen. (Used for damage calculations)
+
+	/*--------------------------------------------------------- Start/Update --------------------------------------------------------- */
+	private void Awake()
+	{
+		Init();
+	}
+
+	/*---------------------------------------------------------- Functions ----------------------------------------------------------- */
+	public void Init()
+	{
+		laneWidth = Screen.width/laneQty;
+		heightFactor = Screen.height/100;
+    
+    lanes = new Lane[laneQty]
+    
+    //Updates lane details accordingly.
+    var i= 1;
+    foreach (Lane lane in lanes)
+    {
+      lane.num = i;
+      lane.pos = i*LaneWidth/2;
+      i++
+    }
+	}
+	public Vector3 LaneWidth
+	{
+    get 
+    {
+    	Vector3 a = Camera.main.ScreenToWorldPoint(Vector3.zero);
+		  Vector3 b = Camera.main.ScreenToWorldPoint(new Vector3(laneWidth, 0, 0));
+		  return (a + b);
+    }
+	}
+	public Vector3 HeightFactor
+	{
+    get
+    {
+		  return Camera.main.ScreenToWorldPoint(new Vector3(0, heightFactor, 0));
+    }
+	}
+}
