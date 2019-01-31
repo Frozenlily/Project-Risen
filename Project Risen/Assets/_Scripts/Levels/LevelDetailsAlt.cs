a7ca67e7c94769128599b977add74631ca0c8791
Frozenlily <njj1997@outlook.com>
2019-01-31 13:32:31 +0800

    Update LevelDetailsAlt.cs

--- Project Risen/Assets/_Scripts/Levels/LevelDetailsAlt.cs
+++ Project Risen/Assets/_Scripts/Levels/LevelDetailsAlt.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelDetails : MonoBehaviour 
+public class LevelDetailsAlt : MonoBehaviour 
 {
 	/*---------------------------------------------------------- Variables ----------------------------------------------------------- */
 	public enum LevelTypes
