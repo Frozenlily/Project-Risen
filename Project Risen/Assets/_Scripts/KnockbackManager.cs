6a811ed3f3488d45966e3a51b7e4162632bb6291
Frozenlily <njj1997@outlook.com>
2019-05-24 14:50:03 +0800

    Update KnockbackManager.cs

--- Project Risen/Assets/_Scripts/KnockbackManager.cs
+++ Project Risen/Assets/_Scripts/KnockbackManager.cs
@@ -5,24 +5,34 @@ using UnityEngine;
 public class KnockbackManager : MonoBehaviour 
 {
     /*---------------------------------------------------------- Variables ----------------------------------------------------------- */
+    [SerializeField] private float time;
+    [SerializeField] private int pushforce;
+    private Vector3 pushDistance;
+    private Collider2D col;
     
     /*-------------------------------------------------------- Start/Update ---------------------------------------------------------- */
     private void Start()
     {
-        
+        pushDistance = new Vector3(0, pushForce * GameUnits.heightFactor, 0);
     }
     private void Update()
     {
         KnockbackDetected();   
     }
     /*--------------------------------------------------------- Functions ------------------------------------------------------------ */
-	public delegate void KnockbackEventHandler(object source, Vector3 pushDistance, float time);
-	public static event KnockbackEventHandler KnockbackDetected;
+    private void OnTriggerEnter(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            OnKnockbackDetection();
+        }
+    }
+    /*--------------------------------------------------------- Functions ------------------------------------------------------------ */
+    public delegate void KnockbackEventHandler(object source, Vector3 pushDistance, float speed);
+    public static event KnockbackEventHandler KnockbackDetected;
     
     public virtual void OnKnockbackDetection()
     {
-        OnKnockback(pushDistance);
+        KnockbackDetected(pushDistance, speed);
     }
-    
-    
 }
